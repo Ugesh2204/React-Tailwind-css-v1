7a96aa52d68bd2623f7e62c293501b79a7e3a6e3
ugesh <ugesh.roopchand@hotmail.com>
2022-06-22 10:05:53 +0400

    Tailwind responsive Grid/flex/@apply properties

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Content from './components/Content';
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Hero/>
+      <div class="container mx-auto">
+        <Hero/>
+        <Content/>
+      </div>
+    
     </>
   );
 }
